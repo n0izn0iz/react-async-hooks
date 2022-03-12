d7d6fc4cb270cf353b41aad4e230020c3016b82d
Norman Meier <norman@berty.tech>
2022-03-12 11:41:58 +0100

    feat: init
    Signed-off-by: Norman Meier <norman@berty.tech>

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [
+		'@babel/proposal-export-namespace-from',
+		[
+			'module-resolver',
+			{
+				alias: {
+					'@berty-labs': './src/',
+				},
+			},
+		],
+		'@babel/plugin-proposal-numeric-separator',
+	],
+}
